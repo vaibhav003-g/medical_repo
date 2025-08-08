92aedec518612d52bfb22571417d4adb682a0bee
vaibhav003-g <vg952296@gmail.com>
2025-08-08 19:11:33 +0530

    Add files via upload

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,64 +1,72 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-data={
-    "Feature":["Age","Height","Weight","Gender","Systolic blood pressure","Diastolic blood pressure","Cholestrol",
-               "Glucose","Smoking","Alcohol consumption","Physical activity","Presence or absence of cardiovascular disease"],
-
-    "Variable Type":["Objective Feature","Objective Feature","Objective Feature","Objective Feature",
-                     "Examination Feature","Examination Feature","Examination Feature","Examination Feature",
-                     "Subjective Feature","Subjective Feature","Subjective Feature","Target Variable"],
-
-    "Variable":["age","height","weight","gender","ap_hi","ap_lo","cholestrol","gluc","smoke","alco","active","cardio"],
-
-    "Value Type":["int(days)","int(cm)","float(kg)","categorical code","int","int",
-                  "1: normal, 2: above normal, 3: well above normal","1: normal, 2: above normal, 3: well above normal",
-                  "binary","binary","binary","binary"]                 
-
-}
-
-df=pd.DataFrame(data)
+df=pd.read_csv("medical_examination.csv")
 print(df)
 
-l=len(df)
-df["height"]=int(input("enter your height in cm:"))
-df["weight"]=float(input("enter your weight in kg:"))
-df["bmi"]=(df["weight"]/(df["height"]*df["height"]))    
-df["overweight"]=[0 if bmi < 25 else 1 for bmi in df["bmi"]]
+bmi=df["weight"]/np.square(df["height"]/100)
+df["overweight"]= (bmi > 25).astype('uint8')
+
 print(df)
 
-
-df["ap_hi"]=int(input("enter your systolic blood pressure:"))
-df["ap_lo"]=int(input("enter your distolic blood pressure:"))
-c=input("enter your cholestrol:")
-g=input("enter your glucose:")
-s=input("enter your smoking status(1 for yes,0 for no):")
-a=input("enter your alcohol consumption status(1 for yes,0 for no):")
-act=input("enter your physical activity status(1 for yes,0 for no):")
-df["chol"]=c
-df["glu"]=g
-df["sm"]=s
-df["al"]=a
-df["ac"]=act
-df["cholestrol"]=[0 if c=="1" else 1 for c in df["chol"]]
-df["glucose"]=[0 if g=="1" else 1 for g in df["glu"]]
-df["smoking"]=[0 if s=="1" else 1 for s in df["sm"]]
-df["alcohol"]=[0 if a=="1" else 1 for a in df["al"]]
-df["activity"]=[0 if act=="1" else 1 for act in df["ac"]]
+df["cholesterol"]=(df["cholesterol"]!="1").astype('uint8')
+df["gluc"]=(df["gluc"]!="1").astype('uint8')
 print(df)
 
+def draw_cat_plot():
+    columns = [
+      'active',
+      'alco',
+      'cholesterol',
+      'gluc',
+      'overweight',
+      'smoke'
+    ]
+    df_cat=df.melt(id_vars=["cardio"], value_vars=columns, var_name="variable", value_name="value")
+    df_cat = df_cat.reset_index() \
+                .groupby(['variable', 'cardio', 'value']) \
+                .agg('count') \
+                .rename(columns={'index': 'total'}) \
+                .reset_index()
+    fig = sns.catplot(
+        x="variable",
+        y="total",
+        col="cardio",
+        hue="value",
+        data=df_cat,
+        kind="bar").fig
 
-df_heat=df[df['ap_hi'] <= df['ap_lo']]
-df_heat['height']=(df['height'] >= df['height'].quantile(0.025))
-df_heat['height']=(df['height'] < df['height'].quantile(0.975))
-df_heat["weight"]=(df["weight"]>=df["weight"].quantile(0.025))
-df_heat["weight"]=(df["weight"]<df["weight"].quantile(0.975))
-print(df_heat)
 
-corr_matrix = df.corr(numeric_only=True)
-print(corr_matrix)
+    plt.show()
+    return fig
+draw_cat_plot()
 
-sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-plt.title("Correlation Matrix")
-plt.show()
+def draw_heat_map():
+    df_heat = df[
+      (df['ap_lo'] <= df['ap_hi'])
+      & (df['height'] >= df['height'].quantile(0.025))
+      & (df['height'] <= df['height'].quantile(0.975))
+      & (df['weight'] >= df['weight'].quantile(0.025))
+      & (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+
+    # Set up the matplotlib figure
+    fig = plt.figure(figsize=(12,6))
+
+    # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(corr, mask=mask,
+                annot=True, fmt='.1f',
+                center=0, vmin=-0.5, vmax=0.5)
+
+    plt.show()
+    return fig
+draw_heat_map()
